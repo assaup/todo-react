bd00e4ef61358d0e3f24b3f0f16ff1b526c3c460
AssaupSamir <samirassaup@gmail.com>
2026-02-10 10:19:38 +0300

    simple validation form

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import Button from "./Button"
 import Field from "./Field"
 import { TasksContext } from "../context/TasksContext"
@@ -12,21 +12,46 @@ const AddTaskForm = () => {
         newTaskInputRef,
     } = useContext(TasksContext)
 
+    const [error, setError] = useState('')
+    const clearNewTaskTitle = newTaskTitle.trim()
+    const isNewTaskTitleEmpty = clearNewTaskTitle.length === 0
+
     const onSubmit = (event) => {
         event.preventDefault()
-        addTask()
+
+        if(!isNewTaskTitleEmpty){
+            addTask(clearNewTaskTitle)
+        }
     }
+
+    const onInput = (event) => {
+        const { value } = event.target
+
+        const clearValue = value.trim()
+        const hasOnlySpaced = value.length > 0 && clearValue.length === 0
+
+        setNewTaskTitle(value)
+        setError(hasOnlySpaced ? 'The task cannot be empty' : '')
+    }
+
+
     return (
         <form className="todo__form" onSubmit={onSubmit}>
             <Field 
                 className='todo__field'
                 label="New task title"
                 id="new-task"
+                error={error}
                 value={newTaskTitle}
-                onInput={({target}) => setNewTaskTitle(target.value)}
+                onInput={onInput}
                 ref={newTaskInputRef}
             />
-            <Button type="submit">Add</Button>
+            <Button 
+                type="submit"
+                isDisabled={isNewTaskTitleEmpty}
+            >
+                Add
+            </Button>
         </form>
     )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,14 @@ const Button = (props) => {
         type = 'button',
         children,
         onClick,
+        isDisabled
     } = props
     return (
         <button 
             className={`button ${className}`}
             type={type}
             onClick={onClick}
-
+            disabled={isDisabled}
         >
             {children}
         </button>
